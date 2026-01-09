be2ada373f84189c39ae080a01134440af0ab58b
sprkgoyal <Rohit3200@github>
2026-01-10 00:25:15 +0530

    fixed SecurityFilterChain for MCP

--- server/src/main/java/spring/ai/mcp/server/configuration/McpSecurityConfiguration.java
+++ server/src/main/java/spring/ai/mcp/server/configuration/McpSecurityConfiguration.java
@@ -1,9 +1,22 @@
 package spring.ai.mcp.server.configuration;
 
+import org.springaicommunity.mcp.security.server.config.McpServerOAuth2Configurer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class McpSecurityConfiguration {
 
     @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
@@ -12,20 +25,32 @@ public class McpSecurityConfiguration {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                // Open every request on the server
+                .cors(cors -> cors.configurationSource(corsConfiguration()))
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers("/mcp").permitAll();
                     auth.anyRequest().authenticated();
                 })
-                // Configure OAuth2 on the MCP server
                 .with(
-                        McpResourceServerConfigurer.mcpServerOAuth2(),
+                        McpServerOAuth2Configurer.mcpServerOAuth2(),
                         (mcpAuthorization) -> {
-                            // REQUIRED: the issuerURI
                             mcpAuthorization.authorizationServer(issuerUrl);
                         }
                 )
                 .build();
+    }
+
+    private CorsConfigurationSource corsConfiguration() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowCredentials(true);
+        configuration.setExposedHeaders(List.of("Mcp-Session-Id"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 
 }
